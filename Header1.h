ee5c1c9b4f23ae7dd0b1486e43118472af0cdb3f
Vasiliy99 <72315416+Vasiliy99@users.noreply.github.com>
2024-09-09 11:29:05 +0300

    Create Header1.h

---
+++ Header1.h
@@ -0,0 +1,13 @@
+#ifndef HEADER1_H
+#define HEADER1_H
+
+#include<iostream>
+#include<string>
+#include<limits>
+#include<fstream>
+
+std::vector<std::string> Available_Words(const std::string crossword);
+
+char* Available_Words(const char* crossword);
+
+#endif
