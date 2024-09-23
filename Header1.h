c41e0e654b128bac6c16e2179090baf305be16f1
Vasiliy99 <72315416+Vasiliy99@users.noreply.github.com>
2024-09-23 12:32:59 +0300

    Update Header1.h

--- Header1.h
+++ Header1.h
@@ -5,9 +5,14 @@
 #include<string>
 #include<limits>
 #include<fstream>
+#include<vector>
 
-std::vector<std::string> Available_Words(const std::string crossword);
+std::vector <std::string> Available_Words(const std::string& crossword);
 
-char* Available_Words(const char* crossword);
+std::vector <char*> Available_Words(const char* crossword);
+
+void ErVVOD();
+
+void Put_comm();
 
 #endif
